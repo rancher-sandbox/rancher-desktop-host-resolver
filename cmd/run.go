050dd0dc589eb02936b789729af99ba77e05b4a5
Nino Kodabande <nkodabande@suse.com>
2022-03-29 16:37:43 -0700

    Adds cli functionality
    Signed-off-by: Nino Kodabande <nkodabande@suse.com>

---
+++ cmd/run.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2022 SUSE LLC
+1
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"github.com/rancher-sandbox/rancher-desktop-host-resolver/pkg/commands"
+	"github.com/spf13/cobra"
+)
+
+// runCmd represents the run command
+var (
+	addr             string
+	tcpPort, udpPort int
+	ipv6             bool
+	hosts            map[string]string
+	// TODO: add fallback IPs
+	//fallBackIPs      []string
+
+	runCmd = &cobra.Command{
+		Use:   "run",
+		Short: "Runs the host-resolver with a given arguments",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return commands.Start(addr, tcpPort, udpPort, ipv6, hosts)
+		},
+	}
+)
+
+func init() {
+	runCmd.Flags().StringVarP(&addr, "listen-address", "a", "", "Address to listen on, \":dnsPort\" if empty.")
+	runCmd.Flags().IntVarP(&tcpPort, "tcp-port", "t", 0, "TCP port to listen on, if non provided random port will be chosen.")
+	runCmd.Flags().IntVarP(&udpPort, "udp-port", "u", 0, "UDP port to listen on, if non provided random port will be chosen.")
+	runCmd.Flags().BoolVarP(&ipv6, "ipv6", "6", false, "Enable IPv6 address family.")
+	runCmd.Flags().StringToStringVarP(&hosts, "built-in-hosts", "c", map[string]string{}, "List of built-in Cnames to IPv4, IPv6 or IPv4-mapped IPv6 in host.rd.internal=111.111.111.111,com.backend.process=2001:db8::68 format.")
+	rootCmd.AddCommand(runCmd)
+}
